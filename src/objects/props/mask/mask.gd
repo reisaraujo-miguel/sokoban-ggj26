e40dbd164675d2893f7bf45f227aa13d7532455c
reisaraujo-miguel <reisaraujo.miguel@gmail.com>
2026-01-30 13:57:01 -0300

    chore(format)

--- src/objects/props/mask/mask.gd
+++ src/objects/props/mask/mask.gd
@@ -1,20 +1,20 @@
 extends Area2D
 
 var cores: Dictionary = {
-    "red mask": 0,
-    "blue mask": 1,
-    "yellow mask": 2,
-    "green mask": 3,
-    "no mask": 4,
+	"red mask": 0,
+	"blue mask": 1,
+	"yellow mask": 2,
+	"green mask": 3,
+	"no mask": 4,
 }
 
 @export var color: Game.MaskColor = Game.MaskColor.RED
 
 
 func _ready() -> void:
-    $Label.text = cores.find_key(color)
+	$Label.text = cores.find_key(color)
 
 
 func _on_body_entered(body: Node2D) -> void:
-    if body is Player:
-        (body as Player).equip_new_mask(color)
+	if body is Player:
+		(body as Player).equip_new_mask(color)
